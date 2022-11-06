1147af1fbaa4fa70455b0c23d3522409a3a17369
Ahmad Hassan <ahmad.naeemllah@devsinc.com>
2022-11-06 14:10:39 +0500

    * Devise: Setup User Model. * Controller: Extract Controllers and Views. * Routes: Add appropriate Routes. * Seed: Add Admin login

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :user_name, :role, presence: true
+  validates :email, uniqueness: true
+
+  # Enums
+  enum role: {
+    user: 0,
+    mod: 1,
+    admin: 2
+  }, _prefix: true
+
+end
