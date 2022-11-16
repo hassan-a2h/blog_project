ca4f3dc263c7145b7828b9955724efead3968f18
Ahmad Hassan <ahmad.naeemllah@devsinc.com>
2022-11-16 11:11:13 +0500

    Configure users model, add :confirmable and also add relevent fields in users table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
